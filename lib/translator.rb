0841741a747e9019b5fee8e264e486a1f972a2c1
Luoana Chirita <luoana.livadariu@gmail.com>
2018-07-11 08:21:56 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,6 +2,7 @@ require 'yaml'
 
 def load_library(file)
   emoticons_file = YAML.load_file(file)
+  emoticons = {}
   emoticons["get_emoticon"] = {}
   emoticons["get_meaning"] = {}
   return emoticons

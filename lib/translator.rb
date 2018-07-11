2471ae91a00070786a3df71a74c8e810676e9d3a
Luoana Chirita <luoana.livadariu@gmail.com>
2018-07-11 09:39:24 +0000

    emoticon translator

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,53 @@
-# require modules here
+require 'yaml'
 
-def load_library
-  # code goes here
+def load_library(file)
+  emoticons_file = YAML.load_file(file)
+  japanese_emoticons = []
+  emoticons_file.each do |meaning, emot_array|
+    japanese_emoticons.push(emot_array[1])
+  end
+  
+  english_emoticons = []
+  emoticons_file.each do |meaning, emot_array|
+    english_emoticons.push(emot_array[0])
+  end
+  
+  meanings = []
+  emoticons_file.each do |meaning, emot_array|
+    meanings.push(meaning)
+  end
+  
+  emoticons = {}
+  emoticons["get_emoticon"] = {}
+  emoticons["get_meaning"] = {}
+  max_index = meanings.length - 1
+  index = (0..max_index).to_a
+
+  index.each do |i|
+    emoticons["get_meaning"][japanese_emoticons[i]] = meanings[i]
+    emoticons["get_emoticon"][english_emoticons[i]] = japanese_emoticons[i]
+  end
+
+  return emoticons
 end
 
-def get_japanese_emoticon
-  # code goes here
+
+def get_japanese_emoticon(file, emoticon)
+  emoticons = load_library(file)
+  if emoticons["get_emoticon"].has_key? emoticon
+    jap_emoticon = emoticons["get_emoticon"][emoticon]
+    
+  else 
+   "Sorry, that emoticon was not found"
+  end
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file, emoticon)
+  emoticons = load_library(file)
+  if emoticons["get_meaning"].has_key? emoticon
+    eng_meaning = emoticons["get_meaning"][emoticon]
+    
+  else 
+   "Sorry, that emoticon was not found"
+  end
 end

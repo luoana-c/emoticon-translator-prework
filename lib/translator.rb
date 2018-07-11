c1c18ed7fa14e0c75ac0f51e0b5d25e72f8287ba
Luoana Chirita <luoana.livadariu@gmail.com>
2018-07-11 08:46:36 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ def load_library(file)
   emoticons_file = YAML.load_file(file)
   japanese_emoticons = []
   emoticons_file.each do |meaning, emot_array|
-    
+    japanese_emoticons.push(emot_array[1])
   end
   emoticons = {}
   emoticons["get_emoticon"] = {}
